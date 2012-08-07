77db8605dc29b150dec662de6d46a234b9650f70
xaoc <xaoc@tensor.ru>
2012-08-07 20:36:24 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'd8c77064107a7a1b28fe1289d2679d4374a392174b43d391004db33ad23f08f313b0d2f5ad2a58bf562b8ff83a0fe52b1f5bbf28105e48573bf8e8b6d7b050d2'
